be15cae09118a95360c9d2a4d8a6d4c88364fa32
Sejin <kisejin2015@gmail.com>
2022-09-09 15:04:42 -0600

    test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,21 @@
 const Engineer = require("../lib/Engineer");
+
+test("testing engineer objects", () => {
+    const Engineer = new Engineer();
+    expect(typeof engineer).toBe('object')
+});
+
+test("testing engineer github", () => {
+    const engineer = new Engineer()
+    expect(engineer.github).toBe('github')
+})
+
+test("testing engineer role", () => {
+    const engineer = new Engineer()
+    expect(engineer.getSchool()).toBe("school")
+})
+
+test("testing engineer role", () => {
+    const engineer = new Engineer()
+    expect(engineer.getRole()).toBe("role")
+})
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,4 +1,4 @@
-const Engineer = require("../lib/Manager");
+const Manager = require("../lib/Manager");
 
 test("testing manager officeNumber", () => {
     const manager = new Manager();
