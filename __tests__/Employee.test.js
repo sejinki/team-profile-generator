3748eb217e79247938eedfe5989f817c98e6887d
Sejin <kisejin2015@gmail.com>
2022-09-08 20:41:25 -0600

    tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,23 @@
+const Employee = require("../lib/Employee");
+
+test("testing employee objects", () => {
+    const employee = new Employee();
+    expect(typeof employee).toBe('object')
+});
+
+test("testing employee object with name", () => {
+    const employee = new Employee('sejin');
+    expect(employee.name).toBe('sejin')
+})
+test("testing employee object with id", () => {
+    const employee = new Employee ('sejin', '123');
+    expect(employee.id).toBe('123');
+})
+test("testing employee object with email", () => {
+    const employee = new Employee ('sejin', '123', 'this@email.com');
+    expect(employee.email).toBe('this@email.com');
+})
+test("testing employee getName", () => {
+    const employee= new Employee("sejin");
+    expect(employee.getName()).toBe(sejin)
+})
