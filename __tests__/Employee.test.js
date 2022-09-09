b6aff25711fe6707a2ad224d2e53782e5af4e1d2
Sejin <kisejin2015@gmail.com>
2022-09-09 14:12:18 -0600

    class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -9,14 +9,17 @@ test("testing employee object with name", () => {
     const employee = new Employee('sejin');
     expect(employee.name).toBe('sejin')
 })
+
 test("testing employee object with id", () => {
     const employee = new Employee ('sejin', '123');
     expect(employee.id).toBe('123');
 })
+
 test("testing employee object with email", () => {
     const employee = new Employee ('sejin', '123', 'this@email.com');
     expect(employee.email).toBe('this@email.com');
 })
+
 test("testing employee getName", () => {
     const employee = new Employee("sejin");
     expect(employee.getName()).toBe('sejin');
@@ -26,6 +29,7 @@ test("testing employee getId", () => {
     const employee = new Employee ('sejin', '123');
     expect(employee.getId()).toBe('123')
 })
+
 test("testing employee getEmail", () => {
     const employee = new Employee('sejin', '123', 'this@email.com');
     expect(employee.getEmail()).toBe('this@email.com');
